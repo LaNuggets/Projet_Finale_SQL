a448699499ce9e4247ed9441ab08852678d1222e
EdgarCou <edgar.couthouis@ynov.com>
2024-10-21 16:04:30 +0200

    add data to the data base, corriger foreign key referent, preparation allemployee pour recevoir data

--- data.sql
+++ data.sql
@@ -15,16 +15,19 @@ CREATE TABLE IF NOT EXISTS posts (
     Title VARCHAR(25),
     DepartmentId INTEGER,
     PermissionId INTEGER,
+    ReferentId INTEGER,
     SalaryId INTEGER,
     FOREIGN KEY (DepartmentId) REFERENCES department(Id),
     FOREIGN KEY (PermissionId) REFERENCES permission(Id),
-    FOREIGN KEY (SalaryId) REFERENCES salary(Id)
+    FOREIGN KEY (SalaryId) REFERENCES salary(Id),
+    FOREIGN KEY (ReferentId) REFERENCES referent(ReferentId)
 );
 
 CREATE TABLE IF NOT EXISTS referent (
     ReferentId INTEGER,
     PostId INTEGER,
-    FOREIGN KEY (ReferentId) REFERENCES employees(Id)
+    FOREIGN KEY (ReferentId) REFERENCES posts(Id)
+    FOREIGN KEY (PostId) REFERENCES posts(Id)
 );
 
 CREATE TABLE IF NOT EXISTS department (
