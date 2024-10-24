eab5801f8d8bf3537f7001182f2f90e1c106523e
EdgarCou <edgar.couthouis@ynov.com>
2024-10-24 13:04:30 +0200

    add select choice on edit

--- editemployee.go
+++ editemployee.go
@@ -16,18 +16,38 @@ func EditEmployee(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	departments, err := Projet_Final_SQL.GetDepartment()
+	department, err := Projet_Final_SQL.GetDepartment()
     if err != nil {
         log.Printf("\033[31mError getting all departments: %v\033[0m", err)
         http.Error(w, "Internal error, could not retrieve departments.", http.StatusInternalServerError)
         return
     }
 
+	referents, err := Projet_Final_SQL.GetReferents()
+    if err != nil {
+        log.Printf("\033[31mError getting all referents: %v\033[0m", err)
+        http.Error(w, "Internal error, could not retrieve referents.", http.StatusInternalServerError)
+        return
+    }
+	
+	posts, err := Projet_Final_SQL.GetPostsByDepartment()
+    if err != nil {
+        log.Printf("\033[31mError getting all referents: %v\033[0m", err)
+        http.Error(w, "Internal error, could not retrieve referents.", http.StatusInternalServerError)
+        return
+    }
+
     data := struct {
         Departments []Projet_Final_SQL.Department
+		Referents   []Projet_Final_SQL.Referent
+		Posts       []Projet_Final_SQL.Post
+
     }{
-        Departments: departments,
+        Departments: department,
+		Referents:   referents,
+		Posts:       posts,
     }
+
 
 	err = tmpl.Execute(w, data)
 	if err != nil {
